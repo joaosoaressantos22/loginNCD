47644f2a64f283d6e118c9f65306006c4f4548a0
Joao <jotavesantos1@gmail.com>
2024-03-22 15:17:18 -0300

    Primeiro Commit

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <?php include('templates/header.php');?>
+    <img src="Imagens/ncd_ufes_logo-removebg-preview.png">
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,25 @@
+<head>
+    <title>Portal NCD</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+        .brand-text{
+            background: #00b8e6;
+            border-radius: 7px;
+            max-height: 65px;
+        }
+    </style>
+</head>
+    <body class="cyan lighten-40">
+        <nav class = "white z-depth-0">
+            <div class = "container">
+                <a href="#" class="brand-logo brand-text">PORTAL NCD</a>
+                <ul id = "nav-mobile" class="right hide-on-small-and-down">
+                    <li><a href="#" class="btn brand z depth-0">Cadastre-se</a></li>
+            </div>
+            <div class = "container">
+                <ul id = "nav-mobile" class="left hide-on-small-and-down">
+                    <li><a href="#" class="btn brand z depth-0">Fazer Login</a></li>
+            </div>
+            
+        </nav>
